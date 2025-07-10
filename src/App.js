c2118ef7ba4caec9b1676e15843238aced7bb3ef
clara4795 <junghyeon4795@gmail.com>
2025-07-10 18:45:13 +0900

    ✨ 홈화면 랜덤 감상 메모 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import AddBook from './components/AddBook';
-import MyBooks from './components/MyBooks';
-import NotFound from './pages/NotFound';
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<AddBook />} />
-        <Route path="/mybooks" element={<MyBooks />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,30 @@
+import {useState} from "react";
+
 function Home() {
-  return <h1>📖홈화면</h1>;
+
+  const dummyMemos = [
+  "📖 『어린 왕자』 - 다시 봐도 감동이에요.",
+  "📕 『1984』 - 무겁지만 꼭 읽어야 할 책 같아요.",
+  "📘 『데미안』 - 나를 돌아보게 했어요.",
+  "📗 『미움받을 용기』 - 마음이 조금 편해졌어요.",
+  ];
+
+  const getRandomMemo = () => {
+    const index = Math.floor(Math.random() * dummyMemos.length);
+    return dummyMemos[index];
+  };
+
+  const [memo, setMemo] = useState(getRandomMemo());
+  
+  return (
+    <div>
+      <h1>📚 오늘의 감상 메모</h1>
+      <p>{memo}</p>
+      <button onClick={()=>setMemo(getRandomMemo())}>
+        다른 메모 보기
+      </button>
+    </div>
+  )
 }
+
 export default Home;
